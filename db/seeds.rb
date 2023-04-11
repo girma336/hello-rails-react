acd2a7d6932a465592465696e1caf24b0964f932
girma <girmatarekegn.gi@gmail.com>
2023-04-11 18:52:30 +0300

    Add databese of my app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Message.create(content: "Hello")
+Message.create(content: "Good morning")
+Message.create(content: "How are you?")
+Message.create(content: "How are you doing")
+Message.create(content: "Hi Microverse Student")
